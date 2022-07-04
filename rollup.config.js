c6a11d5c8b093dc608a6fc2a7a0b9d4608e636b9
L&J <609069481@qq.com>
2022-07-05 02:25:19 +0800

    npm publish 配置

--- rollup.config.js
+++ rollup.config.js
@@ -27,26 +27,31 @@ export default defineConfig({
   input: 'src/index.ts',
   output: [
     {
-      file: `dist/${libName}.cjs.js`,
-      // commonjs格式
-      format: 'cjs',
-    },
-    {
-      file: `dist/${libName}.es.js`,
-      // es module
-      format: 'es',
-    },
-    {
-      file: `dist/${libName}.umd.js`,
-      // 通用格式可以用于node和browser等多个场景
+      file: `dist/index.js`,
       format: 'umd',
-      // 外部引入的模块需要显式告知使用的三方模块的命名，结合下面的external使用
-      // globals: {
-      //   '@antv/g6': 'G6',
-      // },
-      // 注意如果是umd格式的bundle的话name属性是必须的，这时可以在script标签引入后window下会挂载该属性的变量来使用你的类库方法
-      name: libName,
+      name: 'BaoJson',
     },
+    // {
+    //   file: `dist/${libName}.cjs.js`,
+    //   // commonjs格式
+    //   format: 'cjs',
+    // },
+    // {
+    //   file: `dist/${libName}.es.js`,
+    //   // es module
+    //   format: 'es',
+    // },
+    // {
+    //   file: `dist/${libName}.umd.js`,
+    //   // 通用格式可以用于node和browser等多个场景
+    //   format: 'umd',
+    //   // 外部引入的模块需要显式告知使用的三方模块的命名，结合下面的external使用
+    //   // globals: {
+    //   //   '@antv/g6': 'G6',
+    //   // },
+    //   // 注意如果是umd格式的bundle的话name属性是必须的，这时可以在script标签引入后window下会挂载该属性的变量来使用你的类库方法
+    //   name: libName,
+    // },
   ],
   // 解释同globals配置，这个配置的意思是我简单处理把外部依赖不打包进bundle中，而是前置引入或者作为依赖安装使用
   // external: ['@antv/g6'],
