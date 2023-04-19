b09316229864bf1901dcaf368507c78b1025d272
L&J <609069481@qq.com>
2023-04-19 17:52:17 +0800

    doc

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
 # bao-json
+[![github](https://img.shields.io/github/stars/wtgroup/bao-json.svg?style=social)](https://github.com/wtgroup/bao-json)
+[![npm](https://img.shields.io/npm/v/bao-json)](https://www.npmjs.com/package/bao-json)
+
+
 我是一款有"灵魂"的 JSON 工具.
 
+**支持注释解析**, 解析结果含有注释信息.
+注释是我的灵魂.
 
 ## 使用
 
@@ -9,10 +15,67 @@
 ```js
 import {parse, ParseContext} from "bao-json";
 
-const json = '{"name": "张飞", "age": 18}';
+const json = `
+/** 我是一个json */
+{
+  // 姓名
+  "name": "张飞",
+  "age": 123, // 年龄 
+}`;
+
 const parseContext: ParseContext = parse(json);
-console.log(parseContext);
+// 分离注释, 原始内容 和 纯注释 (去掉注释符号 // 或 /**/). 可选, 需要时调用
+parseContext.parseComment()
+// 避免下面输出json循环引用报错
+parseContext.children[0].parent = null
+parseContext.children[1].parent = null
+console.log(JSON.stringify(parseContext, null, "  "));
 ```
+
+输出:
+```json
+{
+"key": null,
+"value": null,
+"children": [
+    {
+        "key": "name",
+        "value": "张飞",
+        "children": [],
+        "parent": null,
+        "comment": "// 姓名\n",
+        "commentMeta": {
+            "schemaDescriptor": {},
+            "comment": "// 姓名\n",
+            "pureComment": "姓名"
+        },
+        "type": 0
+    },
+    {
+        "key": "age",
+        "value": 123,
+        "children": [],
+        "parent": null,
+        "comment": "// 年龄 \n",
+        "commentMeta": {
+            "schemaDescriptor": {},
+            "comment": "// 年龄 \n",
+            "pureComment": "年龄"
+        },
+        "type": 1
+    }
+],
+"parent": null,
+"comment": "/** 我是一个json */",
+"commentMeta": {
+    "schemaDescriptor": {},
+    "comment": "/** 我是一个json */",
+    "pureComment": "我是一个json "
+},
+"type": 4
+}
+```
+
 
 ### Node
 
--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -1,5 +1,69 @@
 import {parse, ParseContext} from "../../src";
 
+describe('demo', () => {
+  it('1', () => {
+    const json = `
+/** 我是一个json */
+{
+  // 姓名
+  "name": "张飞",
+  "age": 123, // 年龄 
+}`;
+
+    const parseContext: ParseContext = parse(json);
+    // 分离注释, 原始内容 和 纯注释 (去掉注释符号 // 或 /**/). 可选, 需要时调用
+    parseContext.parseComment()
+    // 避免下面输出json循环引用报错
+    parseContext.children[0].parent = null
+    parseContext.children[1].parent = null
+    // console.log(parseContext);
+    console.log(JSON.stringify(parseContext, null, "  "));
+
+    let r = `
+{
+"key": null,
+"value": null,
+"children": [
+    {
+        "key": "name",
+        "value": "张飞",
+        "children": [],
+        "parent": null,
+        "comment": "// 姓名\\n",
+        "commentMeta": {
+            "schemaDescriptor": {},
+            "comment": "// 姓名\\n",
+            "pureComment": "姓名"
+        },
+        "type": 0
+    },
+    {
+        "key": "age",
+        "value": 123,
+        "children": [],
+        "parent": null,
+        "comment": "// 年龄 \\n",
+        "commentMeta": {
+            "schemaDescriptor": {},
+            "comment": "// 年龄 \\n",
+            "pureComment": "年龄"
+        },
+        "type": 1
+    }
+],
+"parent": null,
+"comment": "/** 我是一个json */",
+"commentMeta": {
+    "schemaDescriptor": {},
+    "comment": "/** 我是一个json */",
+    "pureComment": "我是一个json "
+},
+"type": 4
+}
+`
+  })
+})
+
 describe('JSON', () => {
 
   it('JSON.parse()', () => {
@@ -27,6 +91,7 @@ describe('JSON', () => {
   "birthday": "2022-3-27 23:39:45"
 }`;
     const parseContext: ParseContext = parse(json);
+    parseContext.parseComment()
     console.log(parseContext);
 
     expect(true).toBe(true)
